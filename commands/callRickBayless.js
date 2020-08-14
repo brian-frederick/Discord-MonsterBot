09fe17a05a5a05525d14ce34c5ba09e5c902f4f4
Brian Frederick <brian.frederick@rallyhealth.com>
2020-08-13 22:27:18 -0500

    move rickBayless content to new content folder

--- commands/callRickBayless.js
+++ commands/callRickBayless.js
@@ -7,45 +7,11 @@ module.exports = {
     const ddb = require('../utils/dynamodb');
     const moves =  require('../utils/moves');
     const movesHelper = require('../utils/movesHelper');
+    const moveContext = require('../content/callRickBayless');
 
     const hunter = await ddb.getHunter(message.author.id);
 
-    const crbQuestions = ` - Where are they now?\n`
-      + ` - What happens here?\n`
-      + ` - What's about to happen?\n`
-      + ` - Who should I look for?\n`
-      + ` - Where should I go?\n`
-      + ` - What does the arcane almanac say?\n`;
-
-    const crb = {
-      name: 'call Rick Bayless',
-      action: 'called Rick Bayless',
-      modifier: 'sharp',
-      outcome: {
-        fail: {
-          title: 'On a failure',
-          description: 'You make an attention-attracting fail.',
-          image: {
-            url: 'https://media.giphy.com/media/lgcUUCXgC8mEo/giphy.gif'
-          },
-          footer: {
-            text: `Maaaarrk that experience.`
-          }
-        },
-        success: {
-          title: 'On a 7-9 Call Rick Bayless, hold 1.',
-          description: `One hold can be spent to ask Rick Bayless one of the following questions:\n` + crbQuestions
-        },
-        high: {
-          title: 'On a 10+ Call Rick Bayless, hold 2',
-          description: `Hold 2. One hold can be spent to ask Rick Bayless one of the following questions:\n` + crbQuestions
-        },
-        advanced: {
-          title: 'On a 12+ Call Rick Bayless',
-          description: `You may ask Rick Bayless any question you want about the mystery, not just the listed ones.\n` + crbQuestions
-        }
-      }
-    };
+    const crb = moveContext['crb'];
 
     let modifiers = [{
       key: 'luck used', 
