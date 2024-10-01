8f151f30541cbc648ed29aa204ab2a58b6f2ca46
Brian Frederick <briandfrederick@gmail.com>
2024-10-01 05:40:30 -0400

    update log to kick off new build

--- src/index.ts
+++ src/index.ts
@@ -214,7 +214,7 @@ client.on('message', message => {
 
 try {
   console.log('attempting to login with ', token);
-  console.log('newly revised version for reboot');
+  console.log('newly revised version for reboot v2');
   Sentry.captureMessage('Logging in.', 'info');
   client.login(token);
 } catch (error) {
